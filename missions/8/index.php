65a5cf6a0d0189f3ad121ca8788e042cdb860bc2
scopeinfinity <gagan1kumar@ymail.com>
2017-01-25 13:15:51 +0530

    iPhone

---
+++ missions/8/index.php
@@ -0,0 +1,17 @@
+<?php
+require('../../flagcontroller.php');
+mustLogin();
+?>
+<html>
+<head><title>Secret Code</title></head>
+<body>
+	<?php
+	$ua = $_SERVER['HTTP_USER_AGENT'];
+	if(!(preg_match('/Version\/10/i',$ua) && preg_match('/iPhone/i',$ua) ))
+	echo "Sorry Secret Code is only for IPhone 10 Users <br>";
+	else
+	echo "Flag is ". getFlag(8). " <br>";
+	?>
+
+</body>
+</html>
